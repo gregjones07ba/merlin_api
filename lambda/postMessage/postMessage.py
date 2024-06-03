4fb8a599aa132ac4bbfa6b0c0f79dc4f244a6b39
Greg Jones <gregjones@Gregs-MacBook-Pro.local>
2024-06-03 15:41:18 -0700

    Add message to dynamo with fixed sequence number.

--- lambda/postMessage/postMessage.py
+++ lambda/postMessage/postMessage.py
@@ -1,11 +1,26 @@
 import json
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('merlin_main')
+
 def lambda_handler(event, context):
     logger.info(event)
+
+    payload = event['payload']
+    
+    table.put_item(Item={
+        'PK': f"GAME_{event['game']}",
+        'SK': f"MESSAGE_{0}",
+        'id': payload['id'],
+        'user.id': payload['user']['id'],
+        'user.type': payload['user']['type'],
+        'effect': json.dumps(payload['effect'])
+    })
     
     return {
     }
