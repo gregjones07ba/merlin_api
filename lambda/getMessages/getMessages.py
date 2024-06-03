a3c9f5513bb314efec48e3c66e8f926a32113590
Greg Jones <gregjones@Gregs-MacBook-Pro.local>
2024-06-03 13:06:24 -0700

    We need an id for the message.

--- lambda/getMessages/getMessages.py
+++ lambda/getMessages/getMessages.py
@@ -10,6 +10,7 @@ def lambda_handler(event, context):
     return {
         'messages': [
             {
+                'id': 0,
                 'user': 12,
                 'effect': {
                     'text': 'I cast Fireball!'
