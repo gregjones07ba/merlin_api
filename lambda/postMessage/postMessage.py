2db05a8bb621708a483c3a1687a51644fbc7e152
Greg Jones <gregjones@Gregs-MacBook-Pro.local>
2024-06-05 10:23:27 -0700

    Change table structure. Use sequential seq number.

--- lambda/postMessage/postMessage.py
+++ lambda/postMessage/postMessage.py
@@ -1,21 +1,33 @@
 import json
 import logging
 import boto3
+from boto3.dynamodb.conditions import Key
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('merlin_main')
+table = dynamodb.Table('merlin_messages')
 
 def lambda_handler(event, context):
     logger.info(event)
 
     payload = event['payload']
     
+    response = table.query(
+        KeyConditionExpression = Key('game').eq(event['game']),
+        Limit = 1,
+        ScanIndexForward = False
+    )
+    if response['Items']:
+        item = response['Items'][0]
+        seq = item['seq'] + 1
+    else:
+        seq = 0
+    
     table.put_item(Item={
-        'PK': f"GAME_{event['game']}",
-        'SK': f"MESSAGE_{0}",
+        'game': event['game'],
+        'seq': seq,
         'id': payload['id'],
         'user.id': payload['user']['id'],
         'user.type': payload['user']['type'],
