66454a9bfc18718db14459a0cd2e39e78a95a82e
Greg Jones <4853264+gregjones07ba@users.noreply.github.com>
2025-10-18 11:09:52 -0700

    Recursive copy syntax was incorrect.

--- lambda/postMessage/build.sh
+++ lambda/postMessage/build.sh
@@ -2,7 +2,7 @@
 
 poetry sync
 mkdir -p dist
-cp --recursive .venv/lib/python*/site-packages/* dist
+cp -R .venv/lib/python*/site-packages/* dist
 cp src/postMessage/postMessage.py dist
 cd dist
 zip -r ../lambda.zip .
