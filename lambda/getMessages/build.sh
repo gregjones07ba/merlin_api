4816c86ae20b644194e46243d776df63334268f2
Greg Jones <4853264+gregjones07ba@users.noreply.github.com>
2025-10-21 16:42:30 -0700

    Copied over build.sh script.
    Modified script to refer to getMessage.
    
    chmod a+x build.sh
    
    Copied over poetry.toml to set in-project venv mode.
    
    Renamed getMessage.py to be consistent with postMessage.
    
    Changed casing in postMessage's build.sh, although it seemed to work even if the case didn't match.

--- lambda/getMessages/build.sh
+++ lambda/getMessages/build.sh
@@ -3,6 +3,6 @@
 poetry sync
 mkdir -p dist
 cp -R .venv/lib/python*/site-packages/* dist
-cp src/postmessage/postMessage.py dist
+cp src/getmessage/getMessage.py dist
 cd dist
 zip -r ../lambda.zip .
